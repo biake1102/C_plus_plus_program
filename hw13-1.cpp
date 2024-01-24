e92387f1ccec26fa77cadaf0dc38e6dc9b978dc0
biake1102 <tonyhung931102@gmail.com>
2024-01-24 09:29:53 +0800

    1/24 fib

--- hw13-1.cpp
+++ hw13-1.cpp
@@ -2,13 +2,19 @@
 using namespace std;
 //費式數列
 int main(){ 
-    int first = 0, second = 1, third, n;
-    cout << "number?";
+    int first = 0, second = 1, third = 0, n;
+    cout << "number?" << endl;
     cin >> n;
-    for(int i = 2; i < n; i++){
+    if(n == 1 || n == 2){
+        cout << "1";
+    }
+    else{
+        for(int i = 2; i < n + 1; i++){
         third = first + second;
         first = second;
         second = third;
     }
     cout << third;
+    }
+    
 }
